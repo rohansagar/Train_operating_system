96309b088f27bfa6bde3534f6dcf5e251fabc3af
rohan <rohansaga4@gmail.com>
2018-12-04 16:16:45 -0500

    Implemented a basic kernel that is pretty dumb

--- kernel/shell.c
+++ kernel/shell.c
@@ -5,4 +5,15 @@
 
 void start_shell()
 {
+     // initialize the keyboard
+    int window_id = wm_create(10,3,50,17);
+    wm_print(window_id, "TOS Shell \n");
+    wm_print(window_id, "------------------------------");
+    
+    while(1){
+
+        char ch = keyb_get_keystroke(window_id, TRUE);
+        wm_print(window_id, "Got key: %c\n",ch);
+    }
+
 }
