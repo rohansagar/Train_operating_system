e07265ec0a070cdb24637b8a096b90936104ec0b
rohan <rohan@sfs-wifi-dhcp-10-143-230-165.wifi.sfsu.edu>
2018-12-07 14:50:22 -0800

    Refined the code to print hello world by using processes

--- kernel/shell.c
+++ kernel/shell.c
@@ -2,18 +2,34 @@
 
 
 
-
+// this function is called from the main and it creates a shell process
 void start_shell()
 {
-     // initialize the keyboard
-    int window_id = wm_create(10,3,50,17);
-    wm_print(window_id, "TOS Shell \n");
-    wm_print(window_id, "------------------------------");
-    
-    while(1){
+    create_process(shell_process,3,0,"shell");
+ 	resign();
+}    
 
-        char ch = keyb_get_keystroke(window_id, TRUE);
-        wm_print(window_id, "Got key: %c\n",ch);
-    }
+// the shell process
+void shell_process(PROCESS self, PARAM param)
+{
+    char ch;
+    Keyb_Message message;
+    int window_id_1 = wm_create(10,3,50,17);
+    wm_clear(window_id_1);
+    
+    wm_print("Hello world \n");
+
+    wm_print_char(window_id_1,"#");
+    
+    while(1)
+    {
+        msg.key_buffer = &ch;
+	    send(keyb_port, &message);
+    
+        }
+
+
+
+
 
 }
